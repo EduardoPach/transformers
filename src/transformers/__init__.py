9f745066a1dc08281453f94fd8ec7125434f7a40
Pablo Montalvo <39954772+molbap@users.noreply.github.com>
2023-10-19 00:24:11 +0200

    Add fuyu model (#26911)
    * initial commit
    
    * add processor, add fuyu naming
    
    * add draft processor
    
    * fix processor
    
    * remove dropout to fix loading of weights
    
    * add image processing fixes from Pedro
    
    * fix
    
    * fix processor
    
    * add basic processing fuyu test
    
    * add documentation and TODO
    
    * address comments, add tests, add doc
    
    * replace assert with torch asserts
    
    * add Mixins and fix tests
    
    * clean imports
    
    * add model tester, clean imports
    
    * fix embedding test
    
    * add updated tests from pre-release model
    
    * Processor: return input_ids used for inference
    
    * separate processing and model tests
    
    * relax test tolerance for embeddings
    
    * add test for logit comparison
    
    * make sure fuyu image processor is imported in the init
    
    * fix formattingh
    
    * more formatting issues
    
    * and more
    
    * fixups
    
    * remove some stuff
    
    * nits
    
    * update init
    
    * remove the fuyu file
    
    * Update integration test with release model
    
    * Update conversion script.
    
    The projection is not used, as confirmed by the authors.
    
    * improve geenration
    
    * Remove duplicate function
    
    * Trickle down patches to model call
    
    * processing fuyu updates
    
    * remove things
    
    * fix prepare_inputs_for_generation to fix generate()
    
    * remove model_input
    
    * update
    
    * add generation tests
    
    * nits
    
    * draft leverage automodel and autoconfig
    
    * nits
    
    * fix dtype patch
    
    * address comments, update READMEs and doc, include tests
    
    * add working processing test, remove refs to subsequences
    
    * add tests, remove Sequence classification
    
    * processing
    
    * update
    
    * update the conversion script
    
    * more processing cleanup
    
    * safe import
    
    * take out ModelTesterMixin for early release
    
    * more cl;eanup
    
    * more cleanup
    
    * more cleanup
    
    * and more
    
    * register a buffer
    
    * nits
    
    * add postprocessing of generate output
    
    * nits
    
    * updates
    
    * add one working test
    
    * fix test
    
    * make fixup works
    
    * fixup
    
    * Arthur's updates
    
    * nits
    
    * update
    
    * update
    
    * fix processor
    
    * update tests
    
    * passe more fixups
    
    * fix
    
    * nits
    
    * don't import torch
    
    * skip fuyu config for now
    
    * fixup done
    
    * fixup
    
    * update
    
    * oups
    
    * nits
    
    * Use input embeddings
    
    * no buffer
    
    * update
    
    * styling processing fuyu
    
    * fix test
    
    * update licence
    
    * protect torch import
    
    * fixup and update not doctested
    
    * kwargs should be passed
    
    * udpates
    
    * update the impofixuprts in the test
    
    * protect import
    
    * protecting imports
    
    * protect imports in type checking
    
    * add testing decorators
    
    * protect top level import structure
    
    * fix typo
    
    * fix check init
    
    * move requires_backend to functions
    
    * Imports
    
    * Protect types
    
    ---------
    
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>
    Co-authored-by: ArthurZucker <arthur.zucker@gmail.com>
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    Co-authored-by: Lysandre <lysandre@huggingface.co>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -343,6 +343,7 @@ _import_structure = {
     "models.focalnet": ["FOCALNET_PRETRAINED_CONFIG_ARCHIVE_MAP", "FocalNetConfig"],
     "models.fsmt": ["FSMT_PRETRAINED_CONFIG_ARCHIVE_MAP", "FSMTConfig", "FSMTTokenizer"],
     "models.funnel": ["FUNNEL_PRETRAINED_CONFIG_ARCHIVE_MAP", "FunnelConfig", "FunnelTokenizer"],
+    "models.fuyu": ["FUYU_PRETRAINED_CONFIG_ARCHIVE_MAP", "FuyuConfig", "FuyuProcessor"],
     "models.git": ["GIT_PRETRAINED_CONFIG_ARCHIVE_MAP", "GitConfig", "GitProcessor", "GitVisionConfig"],
     "models.glpn": ["GLPN_PRETRAINED_CONFIG_ARCHIVE_MAP", "GLPNConfig"],
     "models.gpt2": ["GPT2_PRETRAINED_CONFIG_ARCHIVE_MAP", "GPT2Config", "GPT2Tokenizer"],
@@ -978,6 +979,7 @@ else:
     _import_structure["models.efficientformer"].append("EfficientFormerImageProcessor")
     _import_structure["models.efficientnet"].append("EfficientNetImageProcessor")
     _import_structure["models.flava"].extend(["FlavaFeatureExtractor", "FlavaImageProcessor", "FlavaProcessor"])
+    _import_structure["models.fuyu"].append("FuyuImageProcessor")
     _import_structure["models.glpn"].extend(["GLPNFeatureExtractor", "GLPNImageProcessor"])
     _import_structure["models.grounding_dino"].extend(["GroundingDINOImageProcessor"])
     _import_structure["models.idefics"].extend(["IdeficsImageProcessor"])
@@ -1871,6 +1873,7 @@ else:
             "load_tf_weights_in_funnel",
         ]
     )
+    _import_structure["models.fuyu"].extend(["FuyuForCausalLM", "FuyuPreTrainedModel"])
     _import_structure["models.git"].extend(
         [
             "GIT_PRETRAINED_MODEL_ARCHIVE_LIST",
@@ -4504,6 +4507,7 @@ if TYPE_CHECKING:
     from .models.focalnet import FOCALNET_PRETRAINED_CONFIG_ARCHIVE_MAP, FocalNetConfig
     from .models.fsmt import FSMT_PRETRAINED_CONFIG_ARCHIVE_MAP, FSMTConfig, FSMTTokenizer
     from .models.funnel import FUNNEL_PRETRAINED_CONFIG_ARCHIVE_MAP, FunnelConfig, FunnelTokenizer
+    from .models.fuyu import FUYU_PRETRAINED_CONFIG_ARCHIVE_MAP, FuyuConfig, FuyuProcessor
     from .models.git import GIT_PRETRAINED_CONFIG_ARCHIVE_MAP, GitConfig, GitProcessor, GitVisionConfig
     from .models.glpn import GLPN_PRETRAINED_CONFIG_ARCHIVE_MAP, GLPNConfig
     from .models.gpt2 import GPT2_PRETRAINED_CONFIG_ARCHIVE_MAP, GPT2Config, GPT2Tokenizer
@@ -5074,6 +5078,7 @@ if TYPE_CHECKING:
         from .models.efficientformer import EfficientFormerImageProcessor
         from .models.efficientnet import EfficientNetImageProcessor
         from .models.flava import FlavaFeatureExtractor, FlavaImageProcessor, FlavaProcessor
+        from .models.fuyu import FuyuImageProcessor
         from .models.glpn import GLPNFeatureExtractor, GLPNImageProcessor
         from .models.grounding_dino import GroundingDINOImageProcessor
         from .models.idefics import IdeficsImageProcessor
@@ -5828,6 +5833,10 @@ if TYPE_CHECKING:
             FunnelModel,
             FunnelPreTrainedModel,
             load_tf_weights_in_funnel,
+        )
+        from .models.fuyu import (
+            FuyuForCausalLM,
+            FuyuPreTrainedModel,
         )
         from .models.git import (
             GIT_PRETRAINED_MODEL_ARCHIVE_LIST,
